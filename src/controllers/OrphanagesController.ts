759c987c078cbed37d29019a7deb57934f1afb26
bielworm <gabrielllassis@gmail.com>
2023-07-26 00:29:01 -0300

    feat: adicionar upload de imagens

--- src/controllers/OrphanagesController.ts
+++ src/controllers/OrphanagesController.ts
@@ -1,6 +1,6 @@
 import { Request, Response } from "express";
 import { getRepository } from "typeorm";
-import Orphanage from "../models/orphanages";
+import Orphanage from "../models/Orphanage";
 
 export default {
   async index(request: Request, response: Response) {
@@ -12,7 +12,7 @@ export default {
   },
 
   async show(request: Request, response: Response) {
-    const { id } = request.params; 
+    const { id } = request.params;
 
     const orphanagesRepository = getRepository(Orphanage);
 
@@ -34,6 +34,11 @@ export default {
 
     const orphanagesRepository = getRepository(Orphanage);
 
+    const requestImages = request.files as Express.Multer.File[];
+    const images = requestImages.map(image => {
+      return { path: image.filename }
+    })
+
     const orphanage = orphanagesRepository.create({
       name,
       latitude,
@@ -42,6 +47,7 @@ export default {
       instructions,
       opening_hours,
       open_on_weekends,
+      images
     });
 
     await orphanagesRepository.save(orphanage);
